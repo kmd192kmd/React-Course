1dc9cb9e2edc87e03305e43dcfdb794a67e3e7ff
kmd192kmd <kmd192kmd@gmail.com>
2026-08-06 22:07:45 +0900

    replace checkout propertys and replace a to Link

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,30 +1,12 @@
-import './checkout-header.css';
+import { CheckoutHeader } from './CheckoutHeader';
 import './CheckoutPage.css';
 
 export function CheckoutPage() {
   return (
     <>
-      <title>Checkout</title>
-  
-      <div class="checkout-header">
-        <div class="header-content">
-          <div class="checkout-header-left-section">
-            <a href="/">
-              <img class="logo" src="images/logo.png" />
-              <img class="mobile-logo" src="images/mobile-logo.png" />
-            </a>
-          </div>
+      <title>Checkout</title>   
 
-          <div class="checkout-header-middle-section">
-            Checkout (<a class="return-to-home-link"
-              href="/">3 items</a>)
-          </div>
-
-          <div class="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
+      <CheckoutHeader />
 
       <div class="checkout-page">
         <div class="page-title">Review your order</div>
